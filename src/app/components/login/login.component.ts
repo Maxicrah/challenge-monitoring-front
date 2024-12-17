0a5740c949b037f06a57f972e3cb5feba1c31c02
MAXII\maxii <maximaxmaxii690@gmail.com>
2024-12-17 18:36:56 -0300

    login design finished

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,33 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+// Formulario Reactivo
+loginForm: FormGroup;
 
+// Control del ojo
+showPassword: boolean = false;
+visible: boolean = true;
+changeType: boolean = true;
+
+constructor(private fb: FormBuilder) {
+  this.loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+}
+
+
+viewPassword() {
+  this.visible =!this.visible;
+  this.changeType =!this.changeType;
+}
 }
